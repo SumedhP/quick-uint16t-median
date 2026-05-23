2e443e978956a487d38c3ef103d646d1f6d3f3ea
aruw-jetson-squirtle <robomstr@uw.edu>
2026-05-22 22:21:49 -0700

    build

--- cpp/python-template-cpp/extension.hpp
+++ cpp/python-template-cpp/extension.hpp
@@ -12,6 +12,5 @@ PYBIND11_MODULE(extension, m) {
   m.doc() = "Fast uint16 median for 2D NumPy arrays";
 
   m.def("median", &median_uint16_2d, py::arg("array"), py::arg("non_zero_threshold") = 0.5,
-        "Return the lower median of a 2D uint16 NumPy array, ignoring zeros.
-         Returns NaN if the proportion of non-zero elements is below the specified threshold or if there are no non-zero elements.");
+        "Return the lower median of a 2D uint16 NumPy array, ignoring zeros. Returns NaN if the proportion of non-zero elements is below the specified threshold or if there are no non-zero elements.");
 }
